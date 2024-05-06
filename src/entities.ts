b53bf7a2d7e0bf317b1b2d4faaa6dfa5dfe61cb4
mark <markpackham1@gmail.com>
2024-05-06 14:25:18 +0100

    Add make player function in entities.ts
    https://youtu.be/rICeqnbzkZk?feature=shared&t=3664

---
+++ src/entities.ts
@@ -0,0 +1,27 @@
+import { KaboomCtx } from "kaboom";
+import { scale } from "./constants";
+
+export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
+  const player = k.make([
+    k.sprite("assets", { anim: "kirbIdle" }),
+    // put hitbox in a way where character does not sink into platform
+    k.area({ shape: new k.Rect(k.vec2(4, 5.9), 8, 10) }),
+    k.body(),
+    k.pos(posX * scale, posY * scale),
+    k.scale(scale),
+    // Kaboom allows double jumps (simular to Kirby flying)
+    k.doubleJump(10),
+    k.health(3),
+    k.opacity(1),
+    {
+      speed: 300,
+      direction: "right",
+      isInhaling: false,
+      isFull: false,
+    },
+    "player",
+  ]);
+
+  // enemy collisions
+  player.onCollide("enemy", async () => {});
+}
