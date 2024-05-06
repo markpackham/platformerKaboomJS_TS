529759b4680ffc079cbedcf1fad66bfab58aab77
mark <markpackham1@gmail.com>
2024-05-06 13:14:30 +0100

    Setup loop for map layers
    https://youtu.be/rICeqnbzkZk?feature=shared&t=2421

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,9 @@ import { scale } from "./constants";
 
 export async function makeMap(k: KaboomCtx, name: string) {
   const mapData = await (await fetch(`/${name}.json`)).json();
+
+  // in pos / position both x & y are 0 so we just enter 0 once
+  const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
+
+  const spawnPoints: { [key: string]: { x: number; y: number } } = {};
 }
