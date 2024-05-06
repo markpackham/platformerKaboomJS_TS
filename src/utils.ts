99553d53d1a695d08ee7e8386bab5b3d6cdb5950
mark <markpackham1@gmail.com>
2024-05-06 13:37:01 +0100

    Add collisionIgnore for map
    https://youtu.be/rICeqnbzkZk?feature=shared&t=2607

--- src/utils.ts
+++ src/utils.ts
@@ -8,4 +8,18 @@ export async function makeMap(k: KaboomCtx, name: string) {
   const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)]);
 
   const spawnPoints: { [key: string]: { x: number; y: number } } = {};
+
+  for (const layer of mapData.layers) {
+    if (layer.name === "colliders") {
+      for (const collider of layer.objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(2), collider.width, collider.height),
+            // we use the exit tag for the exit door
+            collisionIgnore: ["platform", "exit"],
+          }),
+        ]);
+      }
+    }
+  }
 }
