dfd6fa3d65d4875cf26a8f3d34df596ce6fbe3ea
mark <markpackham1@gmail.com>
2024-05-13 11:59:29 +0100

    Add enemy entities
    https://youtu.be/rICeqnbzkZk?si=JjYygrhQhYyN6d2W&t=5927

--- src/entities.ts
+++ src/entities.ts
@@ -204,3 +204,108 @@ export function setControls(k: KaboomCtx, player: GameObj) {
     }
   });
 }
+
+// Enemies
+export function makeFlameEnemy(k: KaboomCtx, posX: number, posY: number) {
+  const flame = k.add([
+    k.sprite("assets", { anim: "flame" }),
+    k.scale(scale),
+    k.pos(posX * scale, posY * scale),
+    k.area({
+      shape: new k.Rect(k.vec2(4, 6), 8, 10),
+      collisionIgnore: ["enemy"],
+    }),
+    k.body(),
+    k.state("idle", ["idle", "jump"]),
+    "enemy",
+  ]);
+
+  makeInhalable(k, flame);
+
+  flame.onStateEnter("idle", async () => {
+    await k.wait(1);
+    flame.enterState("jump");
+  });
+
+  flame.onStateEnter("jump", async () => {
+    flame.jump(1000);
+  });
+
+  flame.onStateUpdate("jump", async () => {
+    if (flame.isGrounded()) {
+      flame.enterState("idle");
+    }
+  });
+
+  return flame;
+}
+
+export function makeGuyEnemy(k: KaboomCtx, posX: number, posY: number) {
+  const guy = k.add([
+    k.sprite("assets", { anim: "guyWalk" }),
+    k.scale(scale),
+    k.pos(posX * scale, posY * scale),
+    k.area({
+      shape: new k.Rect(k.vec2(2, 3.9), 12, 12),
+      collisionIgnore: ["enemy"],
+    }),
+    k.body(),
+    k.state("idle", ["idle", "left", "right", "jump"]),
+    { isInhalable: false, speed: 100 },
+    "enemy",
+  ]);
+
+  makeInhalable(k, guy);
+
+  guy.onStateEnter("idle", async () => {
+    await k.wait(1);
+    guy.enterState("left");
+  });
+
+  guy.onStateEnter("left", async () => {
+    guy.flipX = false;
+    await k.wait(2);
+    guy.enterState("right");
+  });
+
+  guy.onStateUpdate("left", () => {
+    guy.move(-guy.speed, 0);
+  });
+
+  guy.onStateEnter("right", async () => {
+    guy.flipX = true;
+    await k.wait(2);
+    guy.enterState("left");
+  });
+
+  guy.onStateUpdate("right", () => {
+    guy.move(guy.speed, 0);
+  });
+
+  return guy;
+}
+
+export function makeBirdEnemy(
+  k: KaboomCtx,
+  posX: number,
+  posY: number,
+  speed: number
+) {
+  const bird = k.add([
+    k.sprite("assets", { anim: "bird" }),
+    k.scale(scale),
+    k.pos(posX * scale, posY * scale),
+    k.area({
+      shape: new k.Rect(k.vec2(4, 6), 8, 10),
+      collisionIgnore: ["enemy"],
+    }),
+    k.body({ isStatic: true }),
+    k.move(k.LEFT, speed),
+    k.offscreen({ destroy: true, distance: 400 }),
+    "enemy",
+  ]);
+
+  makeInhalable(k, bird);
+
+  return bird;
+}
